9d712f82dca66dff238944ce3b73dffabc0ada29
VernalUmbrella <vernalumbrella@gmail.com>
2024-10-01 00:01:56 -0400

    Add laser tracking for Tower class

--- scenes/tower/tower.gd
+++ scenes/tower/tower.gd
@@ -12,4 +12,34 @@ extends Node2D
 @export var damage_per_second: float
 
 @onready var sprite: Sprite2D = $Sprite
+@onready var range: Area2D = $Range
 @onready var range_shape: CollisionShape2D = $Range/CollisionShape2D
+@onready var laser: Line2D = $Laser
+
+var current_target: Enemy
+
+func _ready() -> void:
+	laser.add_point(Vector2.ZERO)
+	laser.add_point(Vector2.ZERO)
+
+func _process(delta: float) -> void:
+	locate_target()
+	attack(delta)
+
+func locate_target() -> void:
+	var candidates: Array[Enemy]
+	for overlapper: Area2D in range.get_overlapping_areas():
+		var enemy: Enemy = overlapper.get_parent() #TODO: rework to avoid get_parent?
+		if enemy.is_in_group("enemies"):
+			candidates.append(enemy)
+	if not candidates:
+		return
+	current_target = candidates[0] #TODO: get based on path progress
+
+func attack(delta: float) -> void:
+	if not current_target:
+		laser.visible = false
+		return
+	laser.visible = true
+	laser.points[1] = current_target.global_position - global_position
+	current_target.current_health -= (delta * damage_per_second)
