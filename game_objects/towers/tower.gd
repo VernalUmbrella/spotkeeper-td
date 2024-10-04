88dda7a8bb76b893b56fb370250649bcc5cd535d
VernalUmbrella <vernalumbrella@gmail.com>
2024-10-03 22:50:38 -0400

    Add range indicator when hovering Tower

--- game_objects/towers/tower.gd
+++ game_objects/towers/tower.gd
@@ -6,7 +6,12 @@ extends Node2D
 @onready var sprite: Sprite2D = $Sprite
 @onready var range_area: Area2D = $AttackRange
 @onready var range_shape: CollisionShape2D = $AttackRange/CollisionShape2D
+@onready var range_indicator: Panel = $RangeIndicator
+@onready var tile_area: Area2D = $Tile
 
+var tween: Tween
+var show_range: bool = false
+var range_fade_time: float = 0.2
 var current_targets: Array[Enemy]
 
 func _ready() -> void:
@@ -33,4 +38,20 @@ func _attack(_delta: float) -> void:
 
 func _on_tower_stats_changed():
 	sprite.texture = tower_stats.texture
-	range_shape.shape.set_size(Main.TILE_SIZE * (1+2*tower_stats.attack_range))
+	var range_size: Vector2 = Main.TILE_SIZE * (1+2*tower_stats.attack_range)
+	range_shape.shape.set_size(range_size)
+	range_indicator.size = range_size
+	range_indicator.set_position(-range_size / 2)
+
+
+func _on_tile_mouse_entered() -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(range_indicator, "modulate", Color.WHITE, range_fade_time)
+
+func _on_tile_mouse_exited() -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(range_indicator, "modulate", Color.TRANSPARENT, range_fade_time)
